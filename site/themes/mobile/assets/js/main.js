e4dc877039ee78274f003c235324078300f04a77
jayven <kassarar@gmail.com>
2021-08-11 11:54:42 +0800

    添加 site 目录

---
+++ site/themes/mobile/assets/js/main.js
@@ -0,0 +1,17 @@
+const juice = require('juice/client')
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  console.log('doc loaded')
+  document.getElementById('copy-wechat').addEventListener('click', e => {
+    let text = juice(document.getElementById('markdown-main').innerHTML)
+
+    const type = 'text/plain'
+    const blob = new Blob([text], { type })
+    console.log(blob)
+    navigator.clipboard.write([new ClipboardItem({ [type]: blob })]).then(() => {
+      alert('拷贝成功, 请使用 chrome/firefox inspect 替换大法粘帖到公众号')
+    }, (e) => {
+      alert(`拷贝失败 ${e}`)
+    })
+  })
+})
