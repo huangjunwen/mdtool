e4f52fc248e7da43a95dcb0a5282a9e1b04b7701
jayven <kassarar@gmail.com>
2021-12-29 17:32:36 +0800

    winston 太慢了 pandoc wrapper 暂时不打 log

--- tool/lib/common.js
+++ tool/lib/common.js
@@ -1,19 +1,28 @@
-const winston = require('winston')
 
 const logPath = '/tmp/pandoc-filter.log'
 const sockPath = '/tmp/pandoc-filter.sock'
 const cachePath = '/tmp/pandoc-cache'
 const cacheCleanLockPath = '/tmp/pandoc-cache-clean.lock'
 
-const { combine, timestamp, json } = winston.format
+let _logger = null
+function logger () {
+  if (!_logger) {
+    const winston = require('winston')
+    const { combine, timestamp, json } = winston.format
+    _logger = winston.createLogger({
+      level: 'info',
+      format: combine(timestamp(), json()),
+      transports: [
+        new winston.transports.File({ filename: logPath, level: 'info' })
+      ]
+    })
+  }
+  return _logger
+}
 
-const logger = winston.createLogger({
-  level: 'info',
-  format: combine(timestamp(), json()),
-  transports: [
-    new winston.transports.File({ filename: logPath, level: 'info' })
-  ]
-})
+function sleep (interval) {
+  return new Promise((resolove) => setTimeout(resolove, interval))
+}
 
 module.exports = {
   logPath,
@@ -21,4 +30,5 @@ module.exports = {
   cachePath,
   cacheCleanLockPath,
   logger,
+  sleep,
 }
